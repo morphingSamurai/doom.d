d360ae7125cac8bb2e68fd5e3e4b59a861a08e60
samurai <bartelloni.guglielmo@gmail.com>
2021-03-23 18:01:42 +0100

    Aggiunto org-roam-server

--- config.el
+++ config.el
@@ -62,3 +62,18 @@
 (map! :leader
       :desc "binding per org roam update capture"
       "r c" #'org-roam-capture)
+
+(use-package org-roam-server
+  :ensure t
+  :config
+  (setq org-roam-server-host "127.0.0.1"
+        org-roam-server-port 8070
+        org-roam-server-authenticate nil
+        org-roam-server-export-inline-images t
+        org-roam-server-serve-files nil
+        org-roam-server-served-file-extensions '("pdf" "mp4" "ogv")
+        org-roam-server-network-poll t
+        org-roam-server-network-arrows nil
+        org-roam-server-network-label-truncate t
+        org-roam-server-network-label-truncate-length 60
+        org-roam-server-network-label-wrap-length 20))
